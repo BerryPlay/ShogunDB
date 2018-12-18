5f3984594c82d309a9f72b67ceed9b7a5aad935f
Berend Campen <campen.berend@gmail.com>
2018-12-18 22:59:10 +0100

    Fixed the user authentication
    Worked on #8

--- src/main/java/de/shogundb/configuration/SecurityConfiguration.java
+++ src/main/java/de/shogundb/configuration/SecurityConfiguration.java
@@ -3,12 +3,15 @@ package de.shogundb.configuration;
 import de.shogundb.authentication.TokenFilter;
 import de.shogundb.authentication.UserDetailsServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
@@ -22,6 +25,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         this.userDetailsService = userDetailsService;
     }
 
+    /**
+     * Configures which routes will not be covered by the security chain. Also enables cross-origin access.
+     *
+     * @param http the http security object
+     * @throws Exception thrown, if any kind of error appears
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -29,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/token").permitAll()
-                .antMatchers(HttpMethod.HEAD,"/token/**").permitAll()
+                .antMatchers(HttpMethod.HEAD, "/token/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/user").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -41,8 +50,36 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.cors();
     }
 
+    /**
+     * Provides a user detail service.
+     *
+     * @return a user detail service
+     */
     @Override
     public UserDetailsService userDetailsService() {
         return this.userDetailsService;
     }
+
+    /**
+     * Provides a authentication manager bean (used to authenticate the user).
+     *
+     * @return an authentication bean
+     * @throws Exception thrown, if any kind of errors appears while authenticating
+     */
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    /**
+     * Define the password encoder to used.
+     *
+     * @return a password encoder
+     */
+    @Bean
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
 }
